aa327b68b3350f28f7e44d9a4e9bf23eed12213c
safy97 <mostafasafy1997@gmail.com>
2019-05-01 11:39:35 +0000

    add stocks list

---
+++ app/controllers/user_stocks_controller.rb
@@ -0,0 +1,19 @@
+class UserStocksController < ApplicationController
+  def create
+    stock = Stock.find_by_ticker(params[:stock_ticker])
+    if stock.blank?
+      stock = Stock.new_from_lookup(params[:stock_ticker])
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:success] = "stock #{stock.ticker} is added"
+    redirect_to my_portfolio_path
+  end
+  def destroy
+    stock = Stock.find(params[:id])
+    @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    @user_stock.destroy
+    flash[:danger] = "stock #{stock.ticker} deleted"
+    redirect_to my_portfolio_path
+  end
+end
